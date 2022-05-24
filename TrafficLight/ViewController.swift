678f4a13cef321cd6f40025a3463c0f9b985d09a
Olga Yurchuk <olga.yurchuk@xored.com>
2022-05-24 22:05:03 +0700

    Logic is added

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -17,12 +17,32 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         redLightView.layer.cornerRadius = 90
         yellowLightView.layer.cornerRadius = 90
         greenLightView.layer.cornerRadius = 90
+        
+        redLightView.alpha = 0.5
+        yellowLightView.alpha = 0.5
+        greenLightView.alpha = 0.5
     }
 
     @IBAction func switchColorButtonPress(_ sender: Any) {
+        if redLightView.alpha == 0.5 && yellowLightView.alpha == 0.5 && greenLightView.alpha == 0.5 {
+            switchColorButton.setTitle("Next", for: .normal)
+            redLightView.alpha = 1.0
+        } else if redLightView.alpha == 1.0 {
+            redLightView.alpha = 0.5
+            yellowLightView.alpha = 1.0
+            greenLightView.alpha = 0.5
+        } else if yellowLightView.alpha == 1.0 {
+            yellowLightView.alpha = 0.5
+            greenLightView.alpha = 1.0
+        } else if greenLightView.alpha == 1.0 {
+            greenLightView.alpha = 0.5
+            redLightView.alpha = 1.0
+        }
+
     }
     
 }
